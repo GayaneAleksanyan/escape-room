075d7af26ecd34a742c29de65e5eb17e75873dc8
Gayane Aleksanyan <galexanyan@gmail.com>
2025-04-29 15:49:57 +0400

    logistic_regression

---
+++ logistic_regression.py
@@ -0,0 +1,9 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+model = LogisticRegression()
+pipe = Pipeline([
+    ('scaler', StandardScaler()),
+    ('classifier', model)
+])
+pipe.fit(X_train, y_train)
